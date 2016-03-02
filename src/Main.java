e04d89f857594da61307d79035ada54765d21b3b
Andrey Tatarenko <andrey17076@gmail.com>
2016-03-02 23:04:41 +0300

    Made error catching more simply, some small fixes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.*;
@@ -7,82 +6,68 @@ import java.util.*;
 public class Main {
     private static final int SELF_OPT = 0;
     private static final int EXIT_OPT = 1;
-    private static final int IO_ERROR = 2;
     private static final Scanner scanIn = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         UDPClient client = new UDPClient();
         client.start();
 
         int userOption;
         System.out.printf("Usage: 0 - start test; 1 - exit\n");
         do {
-            try {
-                userOption = Integer.parseInt(scanIn.nextLine());
-                if (userOption == SELF_OPT)
-                    client.startTesting();
-            } catch (NumberFormatException e) {
-                userOption = IO_ERROR;
-                System.err.println(e);
-            }
+            userOption = Integer.parseInt(scanIn.nextLine());
+            if (userOption == SELF_OPT)
+                client.startTesting();
         } while (userOption != EXIT_OPT);
     }
 }
 
 class UDPClient extends Thread {
-    protected DatagramSocket socket;
-    protected byte[] buffer;
-    protected InetAddress address;
     private static final int PORT = 5555;
     private static final int BUFFER_SIZE = 1000;
     private static final int PACKAGE_QUAT = 1000;
     private static final int FIVE_SECONDS = 5000;
+    private static final int START_MSG = 1;
+    private static final int END_MSG = 2;
 
-    public UDPClient() {
+    protected DatagramSocket socket;
+    protected DatagramPacket packet;
+    protected byte[] buffer;
+    protected InetAddress address;
+
+    public UDPClient() throws SocketException, UnknownHostException {
         super("UDP CLIENT THREAD");
+
         buffer = new byte[BUFFER_SIZE];
-
-        try {
-            address = InetAddress.getByName("10.211.55.19");
-        } catch (UnknownHostException e) {
-            System.err.println(e);
-        }
-
-        try {
-            socket = new DatagramSocket(PORT);
-            socket.setBroadcast(true);
-        } catch (SocketException e) {
-            System.err.println(e);
-        }
+        address = InetAddress.getByName("10.211.55.19");
+        socket = new DatagramSocket(PORT);
     }
 
-    public void startTesting() {
+    public void startTesting() throws IOException, InterruptedException{
         buffer = new byte[buffer.length];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PORT);
 
-        try {
-            socket.send(packet);
-        } catch (IOException e) {
-            System.err.println(e);
-        }
+        buffer[0] = START_MSG;
+        packet = new DatagramPacket(buffer, buffer.length, address, PORT);
+        socket.send(packet);
 
-        try {
-            Thread.sleep(FIVE_SECONDS);
-        } catch (InterruptedException e) {
-            System.err.println(e);
-        }
+        buffer = new byte[buffer.length];
+        packet = new DatagramPacket(buffer, buffer.length, address, PORT);
 
         for (int i = 0; i < PACKAGE_QUAT; i++) {
-            try {
-                socket.send(packet);
-            } catch (IOException e) {
-                System.err.println(e);
-            }
+            socket.send(packet);
         }
+
+        Thread.sleep(FIVE_SECONDS);
+
+        buffer[0] = END_MSG;
+        packet = new DatagramPacket(buffer, buffer.length, address, PORT);
+        socket.send(packet);
+
     }
 
+    @Override
     public void run() {
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        packet = new DatagramPacket(buffer, buffer.length);
         while (true) {
             try {
                 socket.receive(packet);
